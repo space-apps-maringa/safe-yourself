01883c5aac919e383471beaf44d90f8bd385079b
Daniel Fernando Lourusso <dflourusso@gmail.com>
2018-10-20 21:51:22 -0300

    statusBar

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 import Main from './src/navigators/Main'
+
+StatusBar.setBarStyle('light-content', true)
 
 export default () => <Main />
